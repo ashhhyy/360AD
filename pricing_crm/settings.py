2838450d3cccae4cb6fd3b79b1d574329d595a84
ashhhyy <111415268+ashhhyy@users.noreply.github.com>
2026-09-01 23:28:16 +0800

    Update settings.py

--- pricing_crm/settings.py
+++ pricing_crm/settings.py
@@ -75,8 +75,6 @@ TIME_ZONE = "Asia/Manila"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
